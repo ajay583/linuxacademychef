09526e9c15273ebb663498df8a7d08a0b68e183f
root <root@ziras1.mylabserver.com>
2017-01-21 19:19:05 +0000

    commit chef-repo for first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["ziras2"] = { "port" => 80, "domine" => "ziras2.mylabserver.com" }
+default["apache"]["sites"]["ziras2b"] = { "port" => 80, "domine" => "ziras2b.mylabserver.com" }
+
